0949878114ab5b5c77f09561cdb1988f0115f662
Glitch (candied-ceramic) <none>
2019-05-02 06:14:54 +0000

    🌌🏝 Checkpoint ./server.js:4202090/13004

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+
+var parser = require("body-parser");
+app.use(parser.json());
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,7 +29,7 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
