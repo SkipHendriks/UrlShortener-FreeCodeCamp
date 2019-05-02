8efb58910ff64c7775f7e61b86fe9c6b7b54777b
Glitch (candied-ceramic) <none>
2019-05-02 10:18:59 +0000

    🌎🌃 Checkpoint ./server.js:4202090/1006

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var dns = require('dns');
-var URL = require('url');
+const express = require('express');
 
-var cors = require('cors');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 
-var app = express();
+const Url = require('./models/url'); // mongoose model
+
+const dns = require('dns'); // to resolve hostname and partialy verify url corectness
+const URL = require('url').URL; // native node url parser
+
+const cors = require('cors');
+
+const app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }).catch(function (err) {
@@ -25,7 +29,7 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-var parser = require("body-parser");
+const parser = require("body-parser");
 app.use(parser.json());
 
 
@@ -40,9 +44,7 @@ app.get('/', function(req, res){
 });
 
 
-var Url = require('./models/url');
-
-// your first API endpoint... 
+// [POST] endpoint for new urls 
 app.post("/api/shorturl/new", function (req, res) {
   const hostname = new URL(req.body.url).hostname;
   dns.lookup(hostname, (err) => {
@@ -53,13 +55,26 @@ app.post("/api/shorturl/new", function (req, res) {
       let url = new Url({url: req.body.url, short_url: generateUniqueNum()});
       url.save((err) => {
         if (err) {
-          res.status(500).json({error: "serverError"})
+          res.status(500).json({error: "serverError"});
         }
         res.json({url: url.url, short_url: url.short_url});
       });
     }
   });
 });
+
+app.get("/api/shorturl/:num", function (req, res) {
+  Url.findOne({ 'num': req.params.num }, 'original_url', function (err, url) {
+    if (err) {
+      res.status(500).json({error: "serverError"})
+    }
+    if (url) {
+      res.redirect(301, url.original_url);
+    } else {
+      next(new NumNotFoundError());
+    }
+  });
+)};
 
 
 app.listen(port, function () {
